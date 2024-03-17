3a50bab35d4c26ee2e3d40d1e971c855eb375c8d
TrulyMahra <mahraais653@gmail.com>
2024-03-17 17:09:49 +0530

    updated Navbar ui and routing to pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import '../Header/Header.css'
+import { FaUserCircle } from 'react-icons/fa'; 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className="nav">
+        <img src='46069.jpg' alt='logo' height={30} width={30} className='logo'></img>
+        <span className='text'>Lets Make a community</span>
+        <input type='text' placeholder='search' className='search'/>
+       <span className='login-container'>
+        <span className="login-text">Login</span>
+        <FaUserCircle className="login-icon" />
+       </span>
+    </nav>
   )
 }
 
